e5317a565f152a7ffe38725ad2045fac20232fe1
ristokoort <ristokoort@yahoo.com>
2022-09-22 14:45:19 +0300

    Add a data model class

---
+++ MvcCar/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+    }
+}
