ef7382f930efebbe8a942b129926cd3da5ed1e4c
ristokoort <ristokoort@yahoo.com>
2022-09-23 09:07:36 +0300

    Add validation rules to the movie model

--- MvcCar/Models/Car.cs
+++ MvcCar/Models/Car.cs
@@ -10,10 +10,21 @@ namespace MvcCar.Models
     public class Car
     {
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 4)]
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Color { get; set; }
+
+        [Range(1, 300)]
+        [Required]
         public int Speed { get; set; }
 
+        [StringLength(3)]
+        [Required]
         public string Condition { get; set; }
     }
 }
